f71fda1837569b1af67ae801dd8395617160a763
Madeleine Emmons <madeleine.emmons@protonmail.com>
2021-05-05 15:23:46 -0500

    finished sprint 1

--- wayfarer/src/app/app.module.ts
+++ wayfarer/src/app/app.module.ts
@@ -3,7 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import {AppRoutingModule} from './app-routing.module';
 import { CarousalComponent } from './carousal/carousal.component';
-import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
 
@@ -11,7 +11,7 @@ import { NavBarComponent } from './nav-bar/nav-bar.component';
   declarations: [
     AppComponent,
     CarousalComponent,
-    HomeComponent,
+    AboutComponent,
     NavBarComponent
   ],
   imports: [
