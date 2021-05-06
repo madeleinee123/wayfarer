cbbc404f5d162197acdb4d35422495cc76eea1f7
Madeleine Emmons <madeleine.emmons@protonmail.com>
2021-05-06 13:52:44 -0500

    added the post part

--- wayfarer/src/app/home-page/home-page.module.ts
+++ wayfarer/src/app/home-page/home-page.module.ts
@@ -2,18 +2,24 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {CityComponent} from './city/city.component';
 import {HomeComponent} from './home/home.component';
+import {CityInfoComponent} from './city-info/city-info.component';
+import {PostComponent} from './post/post.component';
 
 @NgModule({
   declarations: [
     CityComponent,
-    HomeComponent
+    HomeComponent,
+    CityInfoComponent,
+    PostComponent
   ],
   imports: [
     CommonModule
   ],
   exports: [
     CityComponent,
-    HomeComponent
+    HomeComponent,
+    CityInfoComponent,
+    PostComponent
   ]
 })
 export class HomePageModule { }
---
+++ wayfarer/src/app/home-page/post/post.component.ts
@@ -0,0 +1,19 @@
+import {Component, Input, OnInit} from '@angular/core';
+
+@Component({
+  selector: 'app-post',
+  templateUrl: './post.component.html',
+  styleUrls: ['./post.component.css']
+})
+export class PostComponent implements OnInit {
+  @Input('name') name: string;
+  @Input('content') content: string;
+  @Input('username') username: string;
+  @Input('imageUrl') imageUrl: string;
+  @Input('date') date: string;
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
